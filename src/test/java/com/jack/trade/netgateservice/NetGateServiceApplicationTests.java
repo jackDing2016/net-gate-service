a97b0c0cd63b82fbd64173b93d58dca3bbb528dd
jack <jackding2015@gmail.com>
2021-09-24 02:19:58 -0400

    ccc

---
+++ src/test/java/com/jack/trade/netgateservice/NetGateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.trade.netgateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetGateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
